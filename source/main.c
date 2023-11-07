9b07af6194923df2083e54c1416323d7ad7a0b9f
RyanDeivert555 <rdeivert@icloud.com>
2023-11-07 15:18:32 -0800

    started adding web support

--- source/main.c
+++ source/main.c
@@ -1,37 +1,56 @@
 #include "raylib.h"
 #include "../include/game.h"
-
-// FIX: web build
-#if defined(PLATFORM_WEB)
+#define __EMSCRIPTEN__
+#if defined(__EMSCRIPTEN__)
     #include <emscripten/emscripten.h>
 #endif
 
+// game vars
+GameData game_data;
+GameObjects game_objects;
+TextureManager textures;
+AudioManager audio;
+Fonts fonts;
+
+// TODO: make a struct for music?
+Music music;
+
+void Setup() {
+    game_data = InitGame();
+    game_objects = InitGameObjects();
+    textures = InitTextures();
+    audio = InitAudioManager();
+    fonts = InitFonts();
+    music = LoadMusicStream("resources/blockshock.mod");
+}
+
+void MainLoop() {
+    UpdateGame(&game_data, &game_objects, &audio);
+    UpdateMusicStream(music);
+    BeginDrawing();
+    ClearBackground(WHITE);
+    DrawGame(game_data, textures, game_objects, fonts);
+    DrawFPS(0, 0);
+    EndDrawing();
+}
+
 int main() {
+    const int fps = 60;
     SetTraceLogLevel(LOG_WARNING);
     InitWindow(800, 500, "Arkanoid");
     InitAudioDevice();
-    SetTargetFPS(144);
-
-    GameData game_data = InitGame();
-    GameObjects game_objects = InitGameObjects();
-    TextureManager textures = InitTextures();
-    AudioManager audio = InitAudioManager();
-    Fonts fonts = InitFonts();
-
-    // TODO: make a struct for music?
-    Music music = LoadMusicStream("resources/blockshock.mod");
 
     PlayMusicStream(music);
 
+    #ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(MainLoop, fps, 1);
+
+    #else
+    SetTargetFPS(fps);
     while (!WindowShouldClose()) {
-        UpdateGame(&game_data, &game_objects, &audio);
-        UpdateMusicStream(music);
-        BeginDrawing();
-        ClearBackground(WHITE);
-        DrawGame(game_data, textures, game_objects, fonts);
-        DrawFPS(0, 0);
-        EndDrawing();
+        MainLoop();
     }
+    #endif
 
     UnloadGameTextures(&textures);
     UnloadGameAudio(&audio);
